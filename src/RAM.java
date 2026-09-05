2a9d746833dc9ba313991f8e3e82ad0202e64c88
Srdjan101 <srdjanpaspalj@gmail.com>
2026-09-05 16:39:24 +0200

    Odradjena klasa RAM

--- src/RAM.java
+++ src/RAM.java
@@ -1,26 +1,32 @@
 public class RAM {
-
     private int size;
     private int[] cells;
 
-    public RAM(int size, int[] cells) {
+    public RAM(int size) {
         this.size = size;
-        this.cells = cells;
+        this.cells = new int[size];
+    }
+
+    public int read(int address) {
+        if (address >= 0 && address < size) {
+            return cells[address];
+        }
+        throw new IndexOutOfBoundsException("Adresa van opsega RAM-a: " + address);
+    }
+
+    public void write(int address, int value) {
+        if (address >= 0 && address < size) {
+            cells[address] = value;
+        } else {
+            throw new IndexOutOfBoundsException("Adresa van opsega RAM-a: " + address);
+        }
     }
 
     public int getSize() {
         return size;
     }
 
-    public void setSize(int size) {
-        this.size = size;
-    }
-
     public int[] getCells() {
         return cells;
-    }
-
-    public void setCells(int[] cells) {
-        this.cells = cells;
     }
 }
